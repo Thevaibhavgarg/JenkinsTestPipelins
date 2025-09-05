90113055ac307622c327887f1259838e2a7f67cf
Vaibhav Garg <92752498+Thevaibhavgarg@users.noreply.github.com>
2025-09-05 14:59:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+def sub(a, b):
+    return a - b
+def add(a, b):
+    return a + b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    return a/b
+
+if __name__ == "__main__":
+    print("App running... subtraction(2,3) =", sub(2,3))
+    print("App running... addition(2,3) =", add(2,3))
+    print("App running... multiple(2,3) =", mul(2,3))
+    print("App running... division(2,3) =", div(2,3))
+    
